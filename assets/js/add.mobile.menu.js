bcd1796b3020d2d252cc4e77bab27e62495d6066
dmgame <denis.m.pcspace@gmail.com>
2016-10-03 14:37:37 +0200

    сделал скрипт для меню и проставил его на всех страницах

--- assets/js/add.mobile.menu.js
+++ assets/js/add.mobile.menu.js
@@ -1,21 +1,25 @@
 
-var getMenuWith = $('._header-nav-js').width();
+var getMenuWith = $('.navbar-collapse .navbar-nav').width();
 var getLogoWith = $('.navbar-brand').width();
-
+var getUserMenuWidth = $('.navbar-collapse .navbar-right').width();
 
 $(window).on('load resize', function (){
 	var windowWith = $(window).width();
 
-	var creticalPoint = windowWith - (getMenuWith + getLogoWith + 200);
+	var creticalPoint = windowWith - (getMenuWith + getLogoWith + getUserMenuWidth);
 	
 	if (getMenuWith == 0) return false;
 	
 	if (creticalPoint <= 50) {
-		$('.navbar-collapse').addClass('out out-block');
+		$('.navbar-collapse')
+							.addClass('out out-block')
+							.css('opacity', '1');
 		$('.navbar-header').addClass('in');
 		// return false
 	} else {
-		$('.navbar-collapse').removeClass('out out-block');
+		$('.navbar-collapse')
+							.removeClass('out out-block')
+							.css('opacity', '1');
 		$('.navbar-header').removeClass('in');
 		
 	}
